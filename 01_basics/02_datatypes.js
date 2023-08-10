5158e36b138f4be30f7eadc18d469b85be2abe8a
AsifRehmanArain <58067024+AsifRehmanArain@users.noreply.github.com>
2023-08-10 12:57:41 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   //treat all JS code as newer version
+
+// console.log( 3 + 3 ) console.log("Asif")    // Error Name: SyntaxError: Unexpected identifier 'console'
+
+//If you are not using semi colun to seprate the statement it will be show syntaxError good practice is use new line or seprate statement by semi colun  ( write code as following ) 
+
+console.log( 3 + 3 ) 
+
+console.log("Asif")
+
+
+console.log( 6 + 6 ); 
+console.log("PIAIC");
+
+/*
+
+console.log( 9
+     + 
+     3 
+     )
+
+
+//This is also a bad pratice of coding example the code will compile but code is not retty clear and readable, this type of coding will only work in advance stages in some cases when the  code/statment is in so many lines when the (statement) number of the line is high but code readability shoud be high
+
+*/
