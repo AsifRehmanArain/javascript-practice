f89ae7e00cd7e8544fa30c51ce9fb4150a930636
AsifRehmanArain <58067024+AsifRehmanArain@users.noreply.github.com>
2023-08-11 17:02:47 +0000

    Conversion of data types

---
+++ 01_basics/03_convOper.js
@@ -0,0 +1,34 @@
+/*
+let score = "Asif";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(score);
+console.log(valueInNumber);
+
+//How conversion Operation works
+//"33" =>33
+//"33abc" =>NaN
+//true => 1 ; flase => 0
+
+
+let isLoggedIn = "Asif"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+//"" => false
+// "Asif" => true
+
+
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
