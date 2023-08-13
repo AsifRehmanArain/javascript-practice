ecea37403b3487d2f904b3e82c659c45cd40c156
AsifRehmanArain <58067024+AsifRehmanArain@users.noreply.github.com>
2023-08-13 09:39:02 +0000

    Operations added

--- 01_basics/03_convOper.js
+++ 01_basics/03_convOper.js
@@ -1,4 +1,5 @@
 /*
+
 let score = "Asif";
 
 console.log(typeof score);
@@ -9,6 +10,9 @@ console.log(typeof valueInNumber);
 console.log(score);
 console.log(valueInNumber);
 
+
+
+ 
 //How conversion Operation works
 //"33" =>33
 //"33abc" =>NaN
@@ -25,10 +29,78 @@ console.log(booleanIsLoggedIn);
 // "Asif" => true
 
 
-*/
+
+
+
+
+
 
 let someNumber = 33
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+
+
+
+*/
+
+
+
+
+
+
+/*************************************** Operations ********************************/
+
+/*
+let value = 3
+let negValue = -value
+
+console.log(-value);
+
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);   
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);
+
+
+let str1 = "Hello";
+let str2 = " Asif";
+let str3 = str1 + str2;
+
+console.log(str1+str2);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + "2" + 2);
+console.log(1 + 2 + "2");
+
+console.log("1" - 2);
+console.log(1 - "2");
+console.log("1" - 2 - 2);
+console.log(1 - "2" - 2);
+console.log(1 - 2 - "2");
+
+
+
+*/
+
+
+let gameCounter = 100
+//++gameCounter;
+//gameCounter++
+
+// console.log(gameCounter);
+
+//console.log(++gameCounter);
+
+console.log(gameCounter++);
+console.log(gameCounter);
+
