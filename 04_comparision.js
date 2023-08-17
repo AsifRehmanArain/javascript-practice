2bf20c64b821a118ac2a1ff6ec4a9c5f7be1321a
AsifRehmanArain <58067024+AsifRehmanArain@users.noreply.github.com>
2023-08-17 05:31:39 +0000

    Comparison

---
+++ 04_comparision.js
@@ -0,0 +1,30 @@
+
+
+console.log( 2 > 1);
+console.log( 2 >= 1);
+console.log( 2 < 1);
+console.log( 2 == 1);
+console.log( 2 === 1);
+console.log( 2!= 1);
+
+
+
+/*
+
+These comparison did not used professionally at any stage don't waste your time just go throw
+Comparison and equality is different thing comparing any other datatype is bad habit and confusing
+
+
+console.log( null > 0 );
+console.log( null == 0 );
+console.log( null >= 0 );
+
+
+
+console.log( undefined > 0 );
+console.log( undefined < 0 );
+console.log( undefined == 0 );
+console.log( undefined === 0);
+
+
+*/
